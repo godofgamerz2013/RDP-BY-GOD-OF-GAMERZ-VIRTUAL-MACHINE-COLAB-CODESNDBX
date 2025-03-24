877c12115a7a8d15e8a69c7c7f95c3dbafb1f7c7
godofgamerz2013 <158769016+godofgamerz2013@users.noreply.github.com>
2025-03-24 18:39:48 +0530

    Update RDP-BY-GOD-OF-GAMERZ-VIRTUAL-MACHINE-COLAB-CODESNDBX.py

--- RDP-BY-GOD-OF-GAMERZ-VIRTUAL-MACHINE-COLAB-CODESNDBX.py
+++ RDP-BY-GOD-OF-GAMERZ-VIRTUAL-MACHINE-COLAB-CODESNDBX.py
@@ -142,14 +142,7 @@ except NameError as e:
 
 
 
-bash while true # start an infinite loop
-do
-     echo "GOD.OF-GAMERZ.GREAT-SITE.NET" # print "GOD.OF-GAMERZ.GREAT-SITE.NET" 
-     sleep 1 # wait for 1 second
-done # end the loop
-
-
-
+bash 
 
 
 
